80b99c00ff4525e262886ba46ed0f1cf32ca7d83
Robba-JT <robba.jt@gmail.com>
2015-04-28 18:17:11 +0200

    2015-04-28 - Commit 8

--- db/books.js
+++ db/books.js
@@ -12,10 +12,6 @@ var Q = require("q"),
     },
     reqOption = { "gzip": true };
 
-if (require("ip").address() === "128.1.236.11") {
-    gOptions.proxy = "http://CGDM-EMEA\jtassin:password_4@isp-ceg.emea.cegedim.grp:3128/";
-    reqOption.proxy = "http://CGDM-EMEA\jtassin:password_4@isp-ceg.emea.cegedim.grp:3128/";
-}
 google.options(gOptions);
 
 module.exports.BooksAPI = BooksAPI = function (db) {
