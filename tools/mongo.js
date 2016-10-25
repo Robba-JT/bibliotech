c22dfc853c40b5598d41f26575463598e9664d52
Robba-JT <robba.jt@gmail.com>
2016-10-25 17:47:53 +0200

    jta

--- tools/mongo.js
+++ tools/mongo.js
@@ -22,7 +22,6 @@ module.exports.init = function () {
             if (err) { reject(err); } else {
                 module.exports.client = db;
                 resolve();
-
                 /*if (process.env.NODE_ENV === "production") {
                     var last = (new Date(((new Date()).setDate((new Date()).getDate() - 30)))),
                         defParams = {
