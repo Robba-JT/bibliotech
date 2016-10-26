bd12a970c44668827d6d5f890a70fd1c6f979a4a
Robba-JT <robba.jt@gmail.com>
2016-10-26 11:23:53 +0200

    JTA - Images

--- tools/images.js
+++ tools/images.js
@@ -1,11 +1,12 @@
 const sharp = require("sharp"),
+    console = require("./console"),
     file_type = require("file-type"),
     Q = require("q");
 
-exports = module.exports.reduce = function (photo) {
-    console.log("file type images", file_type(photo));
+exports = module.exports.reduce = function (img) {
+    console.log("file type images", file_type(img));
     return new Q.Promise((resolve, reject) => {
-        sharp(photo)
+        sharp(img)
             .resize(require("nconf").get("config").photos_max_width)
             .embed()
             .withoutEnlargement()
