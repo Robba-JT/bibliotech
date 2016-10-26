e5f5e7d7f1fae5fee79c37f27cf357de9d595520
Robba-JT <robba.jt@gmail.com>
2016-10-26 11:35:33 +0200

    JTA - images

--- tools/images.js
+++ tools/images.js
@@ -3,7 +3,7 @@ const sharp = require("sharp"),
     file_type = require("file-type"),
     Q = require("q");
 
-sharp(require("fs").readFileSync("../root/images/background_login.jpg")).toFile("./temp/output-jpeg-from-buffer.jpg");
+sharp(require("fs").readFileSync("./../root/images/background_login.jpg")).toFile("./../temp/output-jpeg-from-buffer.jpg");
 
 exports = module.exports.reduce = function (img) {
     console.log("file type images", file_type(img));
